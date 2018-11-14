be8de6513400c831a58341c9e1746e66e2afe010
“Nick <amellocello@msn.com>
2018-11-14 03:14:52 -0800

    It will show pagination on leaderboard.

--- ClientApp/src/components/Leaderboard.jsx
+++ ClientApp/src/components/Leaderboard.jsx
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Pager } from 'react-bootstrap';
 
-function Leaderboard(props) {
-  function renderLeaderboardTable(props) {
+class Leaderboard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      tableIndex: 0,
+      shownUsers: []
+    };
+    this.handleRenderLeaderboardTable = this.handleRenderLeaderboardTable.bind(this);
+    this.handleSettingShownUsers = this.handleSettingShownUsers.bind(this);
+    this.handleRenderPagination = this.handleRenderPagination.bind(this);
+    this.handleNextPagination = this.handleNextPagination.bind(this);
+    this.handlePreviousPagination = this.handlePreviousPagination.bind(this);
+  }
+  componentDidMount(){
+    this.handleSettingShownUsers(this.state.tableIndex);
+  }
+  componentWillReceiveProps(){
+    this.handleSettingShownUsers(this.state.tableIndex);
+  }
+  handleSettingShownUsers(index){
+    if(this.props.userList){
+      const users = this.props.userList.slice(index*10, (index + 1)*10);
+      this.setState({
+        shownUsers: users
+      })
+    }
+  }
+  handleNextPagination(){
+    this.handleSettingShownUsers(this.state.tableIndex + 1);
+    this.setState({
+      tableIndex: this.state.tableIndex + 1
+    })
+  }
+  handlePreviousPagination(){
+    this.handleSettingShownUsers(this.state.tableIndex - 1);
+    this.setState({
+      tableIndex: this.state.tableIndex - 1
+    })
+  }
+  handleRenderPagination(){
+    if(this.state.tableIndex === 0){
+      return(
+        <Pager>
+          <Pager.Item next onClick={this.handleNextPagination}>
+            Next
+          </Pager.Item>
+        </Pager>
+      )
+    } else if (this.state.tableIndex >= this.props.userList.length/10 - 1) {
+      return(
+        <Pager>
+          <Pager.Item previous onClick={this.handlePreviousPagination}>
+            Previous
+          </Pager.Item>
+        </Pager>
+      )
+    } else {
+      return (
+        <Pager>
+          <Pager.Item previous onClick={this.handlePreviousPagination}>
+            Previous
+          </Pager.Item>
+          <Pager.Item next onClick={this.handleNextPagination}>
+            Next
+          </Pager.Item>
+        </Pager>
+      )
+    }
+  }
+  handleRenderLeaderboardTable(props) {
     if(props.userList){
       return (
         <table className='table'>
@@ -15,9 +84,9 @@ function Leaderboard(props) {
             </tr>
           </thead>
           <tbody>
-            {props.userList.map((user, index) =>
+            {this.state.shownUsers.map((user, index) =>
               <tr key={user.id}>
-                <td>{index + 1}</td>
+                <td>{this.state.tableIndex*10 + index + 1}</td>
                 <td>{user.name}</td>
                 <td>{user.location}</td>
                 <td>{user.lifetimePoints}</td>
@@ -31,13 +100,17 @@ function Leaderboard(props) {
         )
       }
     }
-  return (
-    <div>
-      <h1>Leaderboard</h1>
-      <p>Top earners in iLink.</p>
-      {renderLeaderboardTable(props)}
-    </div>
-  )
+    render(){
+
+    return (
+      <div>
+        <h1>Leaderboard</h1>
+        <p>Top earners in iLink.</p>
+        {this.handleRenderLeaderboardTable(this.props)}
+        {this.handleRenderPagination()}
+      </div>
+    )
+  }
 }
 
 Leaderboard.propTypes = {
--- ClientApp/src/components/UserPointsForm.jsx
+++ ClientApp/src/components/UserPointsForm.jsx
@@ -6,10 +6,14 @@ class UserPointsForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: null
+      user: null,
+      number: '',
+      reason: ''
     };
     this.handleFindingCurrentUser = this.handleFindingCurrentUser.bind(this);
     this.handleUserPointsFormSubmit = this.handleUserPointsFormSubmit.bind(this);
+    this.handleNumberChange = this.handleNumberChange.bind(this);
+    this.handleReasonChange = this.handleReasonChange.bind(this);
   }
   componentDidMount(){
     this.handleFindingCurrentUser();
@@ -22,10 +26,20 @@ class UserPointsForm extends Component {
     event.preventDefault();
     console.log(this.state);
     this.setState({
-
+      number: '',
+      reason: ''
     })
   }
-
+  handleNumberChange(event) {
+    this.setState({
+      number: event.target.value
+    })
+  }
+  handleReasonChange(event) {
+    this.setState({
+      reason: event.target.value
+    })
+  }
   handleFindingCurrentUser() {
     const userId = this.props.location.split('/')[2];
     const currentUser = this.props.userList.find(user => user.id == userId);
@@ -44,13 +58,20 @@ class UserPointsForm extends Component {
         <hr/>
         <form onSubmit={this.handleUserPointsFormSubmit}>
           <FormGroup>
-            <ControlLabel>Add</ControlLabel>
-            <FormControl value={this.state._name} onChange={this.handleNameChange} type="text" placeholder="a"></FormControl>
+            <ControlLabel>Add/Remove Points</ControlLabel>
+            <FormControl value={this.state.number} onChange={this.handleNumberChange} type="number" placeholder="Add/Remove Points"></FormControl>
           </FormGroup>
+          <FormGroup>
+            <ControlLabel>Reason</ControlLabel>
+            <FormControl value={this.state.reason} onChange={this.handleReasonChange} type="text" placeholder="Reason"></FormControl>
+          </FormGroup>
+          <Button type="submit" bsStyle="success">Submit</Button>
         </form>
+        <hr/>
+
         <style>{`
 
-            `}</style>
+        `}</style>
         </div>
       );
     } else {
